4d04b34f90253383e1a968a5fde11a74a10d8881
ktland <ktland@protonmail.com>
2022-08-31 16:45:33 -0500

    added variable

--- Fall 2022/Python/python_work/full_name.py
+++ Fall 2022/Python/python_work/full_name.py
@@ -1,5 +1,5 @@
 first_name = "ada"
 last_name = "lovelace"
 full_name = f"{first_name} {last_name}"
-message = print(f"Hello, {full_name.title()}!")
+message = f"Hello, {full_name.title()}!"
 print(message)
