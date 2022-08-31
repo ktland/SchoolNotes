dd4f2d2d966c64d2a1167f593107e0853e18f571
ktland <ktland@protonmail.com>
2022-08-31 16:43:48 -0500

    assigned fstring to variable

--- Fall 2022/Python/python_work/full_name.py
+++ Fall 2022/Python/python_work/full_name.py
@@ -1,5 +1,5 @@
 first_name = "ada"
 last_name = "lovelace"
 full_name = f"{first_name} {last_name}"
-print(full_name)
-print(f"Hello, {full_name.title()}!")
+message = print(f"Hello, {full_name.title()}!")
+print(message)
