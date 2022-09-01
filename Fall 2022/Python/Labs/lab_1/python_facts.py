4439ca276c9686cb998bce2a56624b0bc1d1d94c
ktland <ktland@protonmail.com>
2022-08-31 20:57:18 -0500

    added extra newline in things_learned

--- Fall 2022/Python/Labs/lab_1/python_facts.py
+++ Fall 2022/Python/Labs/lab_1/python_facts.py
@@ -7,5 +7,5 @@ introduction_message = f"My name is {first_name.title()} {last_name.title()}\n"
 print(introduction_message)
 
 # List facts that I've learned about Python
-things_learned = "Things I have learned about Python:\n\t Python is a general purpose programming language.\n\t Python was created by Guido van Rossum and released in 1991.\n"
+things_learned = "Things I have learned about Python:\n\n\t Python is a general purpose programming language.\n\t Python was created by Guido van Rossum and released in 1991.\n"
 print(things_learned)
