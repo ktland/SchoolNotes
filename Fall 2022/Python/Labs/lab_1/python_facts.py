9a5ed74bf55a72399fc3e04ab9725780aac06ade
ktland <ktland@protonmail.com>
2022-08-31 21:07:56 -0500

    added variables for Python creator

--- Fall 2022/Python/Labs/lab_1/python_facts.py
+++ Fall 2022/Python/Labs/lab_1/python_facts.py
@@ -8,6 +8,12 @@ last_name = "land"
 introduction_message = f"My name is {first_name.title()} {last_name.title()}\n"
 print(introduction_message)
 
-# List facts that I've learned about Python
-things_learned = "Things I have learned about Python:\n\n\t Python is a general purpose programming language.\n\t Python was created by Guido van Rossum and released in 1991.\n"
+# Define variable for Python creator's name
+creator_first_name = "guido"
+creator_middle_name = "Van"
+creator_last_name = "rossum"
+
+
+# List facts that I've learned about Python using Python creator's variables
+things_learned = f"Things I have learned about Python:\n\n\t Python is a general purpose programming language.\n\t Python was created by {creator_first_name.title()} {creator_middle_name.lower()} {creator_last_name.title()} and released in 1991.\n"
 print(things_learned)
