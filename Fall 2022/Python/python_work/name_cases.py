e1ae3d603f0447c9af82df7f205d974f4c5a496b
ktland <ktland@protonmail.com>
2022-08-31 20:36:15 -0500

    added files

---
+++ Fall 2022/Python/python_work/name_cases.py
@@ -0,0 +1,14 @@
+# Use a variable to represent a person's name, and then print that person's name in lowercase, uppercase, and title case.
+# Title Case
+first_name = "kellen"
+last_name = "land"
+message = f"{first_name.title()} {last_name.title()}"
+print(message)
+# Uppercase
+message_uppercase = f"{first_name.upper()} {last_name.upper()}"
+print(message_uppercase)
+# Lowercase
+first_name_upper = "KELLEN"
+last_name_upper = "LAND"
+message_lowercase = f"{first_name_upper.lower()} {last_name_upper.lower()}"
+print(message_lowercase)
