394478acdd28727b2e5aeeb3909a5d163b13c293
ktland <ktland@protonmail.com>
2022-09-01 09:20:38 -0500

    created file

---
+++ Fall 2022/Python/python_work/famous_quote.py
@@ -0,0 +1,4 @@
+# Set variables for famous person's name
+famous_person = "Salvador Dali"
+quote = "Have no fear of perfection,\n you'll never reach it."
+print(f'{famous_person.title()} once said, "{quote}"')
