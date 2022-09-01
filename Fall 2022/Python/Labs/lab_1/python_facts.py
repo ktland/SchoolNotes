b42d8822c6aa99f64b76dd4bbb6a8d8bf1cbcb86
ktland <ktland@protonmail.com>
2022-08-31 20:54:13 -0500

    created file and code written

---
+++ Fall 2022/Python/Labs/lab_1/python_facts.py
@@ -0,0 +1,11 @@
+# Define variables for person's name
+first_name = "kellen"
+last_name = "land"
+
+# Introduce yourself
+introduction_message = f"My name is {first_name.title()} {last_name.title()}\n"
+print(introduction_message)
+
+# List things that I've learned about Python
+things_learned = "Things I have learned about Python:\n\t Python is a general purpose programming language.\n\t Python was created by Guido van Rossum and released in 1991.\n"
+print(things_learned)
