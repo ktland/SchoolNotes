45739e8ef6151c8e89df965c09f7e90da0e5a17d
ktland <ktland@protonmail.com>
2022-09-07 18:55:53 -0500

    added code

--- Fall 2022/Python/Labs/unit_1/lab_2/total_purchase.py
+++ Fall 2022/Python/Labs/unit_1/lab_2/total_purchase.py
@@ -4,3 +4,21 @@
 # Description:  Lab 2
 # ###############################
 
+# declare constant for sales tax rate and set to 6%
+TAX_RATE = 0.06
+
+# initialize numeric variables to hold price of four sales items
+pants = 175
+shirt = 125
+shoes = 300
+belt = 100
+
+# calc subtotal
+subtotal = pants + shirt + shoes + belt
+print(subtotal)
+# calc sales tax
+sales_tax = subtotal * tax_rate
+print(sales_tax)
+# calc grand total
+grand_total = subtotal + sales_tax
+print(grand_total)
