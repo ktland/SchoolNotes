bfa7829f5f79718f824d43c2fc292fab3edb945d
ktland <ktland@protonmail.com>
2022-09-07 19:33:16 -0500

    added purchased items in a column

--- Fall 2022/Python/Labs/unit_1/lab_2/total_purchase.py
+++ Fall 2022/Python/Labs/unit_1/lab_2/total_purchase.py
@@ -15,10 +15,22 @@ belt = 100
 
 # calc subtotal
 subtotal = pants + shirt + shoes + belt
-print(subtotal)
+
 # calc sales tax
-sales_tax = subtotal * tax_rate
-print(sales_tax)
+sales_tax = subtotal * TAX_RATE
+
 # calc grand total
 grand_total = subtotal + sales_tax
-print(grand_total)
+
+# print program name
+print("Program:  Total Purchase")
+print()
+
+# display single dash line
+print(28 * "-")
+
+# print purchased items
+print(f"Item #1 - Pants: \t${pants:,.2f}")
+print(f"Item #2 - Shirt: \t${shirt:,.2f}")
+print(f"Item #3 - Shoes: \t${shoes:,.2f}")
+print(f"Item #4 - Belt: \t${belt:,.2f}")
