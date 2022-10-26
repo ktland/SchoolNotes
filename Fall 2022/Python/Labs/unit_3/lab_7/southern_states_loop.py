30745718085321ccf8b938d867fad6c14ed3863d
ktland <ktland@protonmail.com>
2022-10-26 20:34:36 +0000

    Oct 26, 2022, 3:34 PM

--- Fall 2022/Python/Labs/unit_3/lab_7/southern_states_loop.py
+++ Fall 2022/Python/Labs/unit_3/lab_7/southern_states_loop.py
@@ -10,19 +10,10 @@ southern_states = ['virginia', 'tennessee', 'arkansas', 'louisiana', 'north caro
 # print name of report
 print("Report - Southern United States\n")
 
-# show unsorted list using index of each element in list - show title case
+# show unsorted list using for in loop - show title case
 print("UNSORTED:\n")
-print(f"{southern_states[0].title()}")
-print(f"{southern_states[1].title()}")
-print(f"{southern_states[2].title()}")
-print(f"{southern_states[3].title()}")
-print(f"{southern_states[4].title()}")
-print(f"{southern_states[5].title()}")
-print(f"{southern_states[6].title()}")
-print(f"{southern_states[7].title()}")
-print(f"{southern_states[8].title()}")
-print(f"{southern_states[9].title()}")
-print(f"{southern_states[10].title()}\n")
+for state in southern_states:
+    print(state.title())
 
 # use neg index to access last element in list and show it in title case
 print(f"Last state on this unsorted list: {southern_states[-1].title()}\n")
