d55925835a2e636759e18811ea2a006a0f810376
ktland <ktland@protonmail.com>
2022-11-01 15:42:47 +0000

    Nov 1, 2022, 10:42 AM

--- Fall 2022/Python/Labs/unit_3/lab_8/rectangle_main.py
+++ Fall 2022/Python/Labs/unit_3/lab_8/rectangle_main.py
@@ -6,3 +6,14 @@
 
 import rectangle_mod
 
+print("Program - Find Area and Perimeter of Rectangle: ")
+
+repeat = True
+
+while repeat:
+    width = input("\nEnter the rectangle's width (inches): ")
+    width = float(width)
+    length = float(input("Enter rectangle's length (inches): "))
+    
+    area = rectangle_mod.find_area(width, length)
+    perimeter = rectangle_mod.find_perimeter(width, length)
