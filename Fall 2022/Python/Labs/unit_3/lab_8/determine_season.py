34857dfc77f1c0fb265a5fd17ebab549859a3456
ktland <ktland@protonmail.com>
2022-11-01 16:06:28 +0000

    Nov 1, 2022, 11:06 AM

--- Fall 2022/Python/Labs/unit_3/lab_8/determine_season.py
+++ Fall 2022/Python/Labs/unit_3/lab_8/determine_season.py
@@ -4,3 +4,23 @@
 # Description:  Lab 8
 ###################################
 
+# function definition determine_season – receives temp and returns season
+def determine_season(input_temp):
+    """Receives temp and returns probable season"""
+    
+    season = ""
+    
+    if input_temp > 130 or input_temp < -20:
+        season = "invalid"
+    elif input_temp >= 90:
+        season = "summer"
+    elif input_temp >= 70 and input_temp < 90:
+        season = "spring"
+    elif input_temp >= 50 and input_temp < 70:
+        season = "fall"
+    elif input_temp < 50:
+        season = "winter"
+    else:
+        season = ""
+    
+    return season
