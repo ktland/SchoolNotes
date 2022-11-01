0fb626310b2fca409b25b8fde0ca2c4136b07c8b
ktland <ktland@protonmail.com>
2022-11-01 15:27:54 +0000

    Nov 1, 2022, 10:27 AM

---
+++ Fall 2022/Python/Labs/unit_3/lab_8/rectangle_main.py
@@ -0,0 +1,6 @@
+# Program:      rectangle_main.py
+# Programmer:   Kellen Land
+# Date:         11/01/2022
+# Description:  Lab 8
+#################################
+
---
+++ Fall 2022/Python/Labs/unit_3/lab_8/rectangle_mod.py
@@ -0,0 +1,6 @@
+# Program:      rectangle_mod.py
+# Programmer:   Kellen Land
+# Date:         11/01/2022
+# Description:  Lab 8
+################################
+
