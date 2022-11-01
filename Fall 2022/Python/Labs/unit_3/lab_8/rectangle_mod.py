bc537b3a486334f55b27a49c941a4e62fc3f3b0f
ktland <ktland@protonmail.com>
2022-11-01 15:34:06 +0000

    Nov 1, 2022, 10:34 AM

--- Fall 2022/Python/Labs/unit_3/lab_8/rectangle_mod.py
+++ Fall 2022/Python/Labs/unit_3/lab_8/rectangle_mod.py
@@ -5,10 +5,12 @@
 ################################
 
 def find_area(width, length):
+    """Calculate the area of a rectangle."""
     area = width * length
     return area
 
 def find_perimeter(width, length):
+    """Calculate the perimeter of a rectangle."""
     perimeter = 2 * (width + length)
     return perimeter
 
