feab0b2970ff883cd80568ecc9b2b7cb209699c4
ktland <ktland@protonmail.com>
2022-11-29 23:56:58 +0000

    Nov 29, 2022, 5:56 PM

--- Fall 2022/Python/Labs/unit_4/lab_10/grades_read.py
+++ Fall 2022/Python/Labs/unit_4/lab_10/grades_read.py
@@ -7,4 +7,7 @@
 filename = 'grades.txt'
 
 with open(filename, mode = 'r') as grades:
-    print(f"{"ID":<10}{"Name":>10}{"Grade":>10}")
+    print(f"{"ID":<10}{"Name":<10}{"Grade":>10}")
+    print('==============================')
+for record in grades:
+    student_id, name, grade = record.split()
