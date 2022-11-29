71e9095cad18a7bdfec7ab8fad81fad4e9f0304e
ktland <ktland@protonmail.com>
2022-11-29 23:50:57 +0000

    Nov 29, 2022, 5:50 PM

--- Fall 2022/Python/Labs/unit_4/lab_10/grades_write.py
+++ Fall 2022/Python/Labs/unit_4/lab_10/grades_write.py
@@ -8,3 +8,7 @@ filename = 'grades.txt'
 
 with open(filename, mode = 'w') as grades:
     grades.write('111 Olivera 90\n')
+    grades.write('222 Benavides 80\n')
+    grades.write('333 Gonzales 70\n')
+    grades.write('444 Casas 60\n')
+    grades.write('555 Ramirez 50\n')
