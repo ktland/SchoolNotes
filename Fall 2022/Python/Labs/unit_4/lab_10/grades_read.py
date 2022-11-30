c2319216470771a51a1428feedc4d25a2fe25760
ktland <ktland@protonmail.com>
2022-11-30 00:18:02 +0000

    Nov 29, 2022, 6:18 PM

--- Fall 2022/Python/Labs/unit_4/lab_10/grades_read.py
+++ Fall 2022/Python/Labs/unit_4/lab_10/grades_read.py
@@ -9,26 +9,28 @@ filename = 'grades.txt'
 count_grades = 0
 sum_grades = 0
 
-with open(filename, mode = 'r') as grades:
-    print(f'{"ID":<10}{"Name":<10}{"Grade":>10}')
-    print('==============================')
-    for record in grades:
-        student_id, name, grade = record.split()
-        grade = int(grade)
-        print(f"{student_id:<10}{name:<10}{grade:>10}")
-        count_grades = count_grades + 1
-        sum_grades = sum_grades + grade
-    print('=' * 30)
+
 
 print(f"Number of Grades: {count_grades}")
 print(f"Average: {sum_grades/count_grades:.2f}")
 
 try:
-
+    with open(filename, mode = 'r') as grades:
+        print(f'{"ID":<10}{"Name":<10}{"Grade":>10}')
+        print('==============================')
+        for record in grades:
+            student_id, name, grade = record.split()
+            grade = int(grade)
+            print(f"{student_id:<10}{name:<10}{grade:>10}")
+            count_grades = count_grades + 1
+            sum_grades = sum_grades + grade
+        print('=' * 30)
 
 except FileNotFoundError:
+    print(f"Sorry, the file {filename} does not exist.")
 
 except:
-
+    print("An error has occurred. Please see admin.")
 else:
-    
+    print(f"Number of Grades: {count_grades}")
+    print(f"Average: {sum_grades/count_grades:.2f}")
