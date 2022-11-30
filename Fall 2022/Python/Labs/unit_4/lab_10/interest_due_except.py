ee40c2ba9662c4bf2e692bac528de1c5ce563430
ktland <ktland@protonmail.com>
2022-11-30 00:33:05 +0000

    Nov 29, 2022, 6:33 PM

--- Fall 2022/Python/Labs/unit_4/lab_10/interest_due_except.py
+++ Fall 2022/Python/Labs/unit_4/lab_10/interest_due_except.py
@@ -12,7 +12,7 @@ repeat = True
 
 # while repeat
 while repeat:
-    amount = input("\nHow many dollars do you wish to borrow? ")
+    '''amount = input("\nHow many dollars do you wish to borrow? ")
     amount = float(amount)
     interest_rate = float(input("What is the interest rate? "))
     years = int(input("How many years would you take the loan? "))
@@ -26,9 +26,9 @@ while repeat:
     
     again = input("\nWould you like to run another calculation? (y/n)")
     if again == 'n':
-        repeat = False
+        repeat = False'''
         
-print("\nThanks for using this program. Goodbye!"
+print("\nThanks for using this program. Goodbye!")
 
 try:
     amount = input("\nHow many dollars do you wish to borrow? ")
@@ -37,4 +37,9 @@ try:
 except ValueError:
     print("Input Error >>> Please enter numbers only. You will have to start again.")
 except:
-    print("An error has occured.")
+    print("An error has occurred.")
+else:
+    total_interest = amount *  (interest_rate/100) * years
+    output = f"\nIf you borrow ${amount:,.2f} at an interest rate of {interest_rate}%"
+    output += f"\nfor {years} years, you will pay ${total_interest:,.2f} in interest"
+    print(output)
