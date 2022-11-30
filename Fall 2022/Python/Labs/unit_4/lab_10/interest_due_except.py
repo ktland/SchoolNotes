7b5a52de20752382d5177ead859d2f2a5a4ec0fd
ktland <ktland@protonmail.com>
2022-11-30 00:27:04 +0000

    Nov 29, 2022, 6:27 PM

---
+++ Fall 2022/Python/Labs/unit_4/lab_10/interest_due_except.py
@@ -0,0 +1,34 @@
+# Program:      interest_due_except.py
+# Programmer:   Kellen Land
+# Date:         11/29/2022
+# Description:  Lab 10
+######################################
+
+# print the name of the program
+print("Program - Calculate Interest on Loan:")
+
+# set repeat flag to True
+repeat = True
+
+# while repeat
+while repeat:
+    amount = input("\nHow many dollars do you wish to borrow? ")
+    amount = float(amount)
+    interest_rate = float(input("What is the interest rate? "))
+    years = int(input("How many years would you take the loan? "))
+    
+    total_interest = amount *  (interest_rate/100) * years
+    
+    output = f"\nIf you borrow ${amount:,.2f} at an interest rate of {interest_rate}%"
+    output += f"\nfor {years} years, you will pay ${total_interest:,.2f} in interest"
+    
+    print(output)
+    
+    again = input("\nWould you like to run another calculation? (y/n)")
+    if again == 'n':
+        repeat = False
+        
+print("\nThanks for using this program. Goodbye!"
+
+try:
+    amount = float(input())
