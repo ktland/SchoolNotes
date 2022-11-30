623c3545e3c0dec79f706153e250afbed4956bfe
ktland <ktland@protonmail.com>
2022-11-30 00:39:07 +0000

    Nov 29, 2022, 6:39 PM

--- Fall 2022/Python/Labs/unit_4/lab_10/interest_due_except.py
+++ Fall 2022/Python/Labs/unit_4/lab_10/interest_due_except.py
@@ -12,7 +12,7 @@ repeat = True
 
 # while repeat
 while repeat:
-    amount = input("\nHow many dollars do you wish to borrow? ")
+    '''amount = input("\nHow many dollars do you wish to borrow? ")
     amount = float(amount)
     interest_rate = float(input("What is the interest rate? "))
     years = int(input("How many years would you take the loan? "))
@@ -22,15 +22,29 @@ while repeat:
     output = f"\nIf you borrow ${amount:,.2f} at an interest rate of {interest_rate}%"
     output += f"\nfor {years} years, you will pay ${total_interest:,.2f} in interest"
     
-    print(output)
+    print(output)'''
     
+    try:
+        amount = input("\nHow many dollars do you wish to borrow? ")
+        amount = float(amount)
+        interest_rate = float(input("What is the interest rate? "))
+        years = int(input("How many years would you take the loan? "))
+    except ValueError:
+        print("Input Error >>> Please enter numbers only. You will have to start again.")
+    except:
+        print("An error has occurred.")
+    else:
+        total_interest = amount *  (interest_rate/100) * years
+        output = f"\nIf you borrow ${amount:,.2f} at an interest rate of {interest_rate}%"
+        output += f"\nfor {years} years, you will pay ${total_interest:,.2f} in interest"
+        print(output)
     again = input("\nWould you like to run another calculation? (y/n)")
     if again == 'n':
         repeat = False
         
 print("\nThanks for using this program. Goodbye!")
 
-try:
+'''try:
     amount = input("\nHow many dollars do you wish to borrow? ")
     amount = float(amount)
     interest_rate = float(input("What is the interest rate? "))
@@ -43,4 +57,4 @@ else:
     total_interest = amount *  (interest_rate/100) * years
     output = f"\nIf you borrow ${amount:,.2f} at an interest rate of {interest_rate}%"
     output += f"\nfor {years} years, you will pay ${total_interest:,.2f} in interest"
-    print(output)
+    print(output)'''
